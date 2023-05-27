e71cdfe6ec8af01a6d0c73c485fb1bcbc5a2ffcb
Mmark98 <58510020+Mmark98@users.noreply.github.com>
2023-05-27 11:07:10 +0200

    Add files via upload

--- BookStorage/library/src/main/java/com/example/library/repository/BookRepository.java
+++ BookStorage/library/src/main/java/com/example/library/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package com.example.library.repository;
 
 import com.example.library.model.Book;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Ez az osztály a Spring Security beállításait konfigurálja.
@@ -9,6 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * használjuk a biztonsági konfigurációhoz.
  */
 
+@Repository
+@SpringBootApplication
 public interface BookRepository
         extends JpaRepository<Book, Long> {
 
